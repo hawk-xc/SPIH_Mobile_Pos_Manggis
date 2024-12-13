b9ca8a204b13f0152f647b8123396893db06db16
Popo-XPC Dev <popoxpc@gmail.com>
2024-12-14 00:13:29 +0700

    sql database

---
+++ db_backup/manggis.sql
@@ -0,0 +1,124 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: manggisdb
+-- ------------------------------------------------------
+-- Server version	9.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_examinations`
+--
+
+DROP TABLE IF EXISTS `tb_examinations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_examinations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `patient_id` int NOT NULL,
+  `body_weight` int DEFAULT NULL,
+  `gestational_age` int DEFAULT '9',
+  `fundal_height` int DEFAULT NULL,
+  `leg_swelling` int DEFAULT NULL,
+  `action_desc` text,
+  `checkup_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `checkup_back_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_patient_examinations` (`patient_id`),
+  CONSTRAINT `fk_patient_examinations` FOREIGN KEY (`patient_id`) REFERENCES `tb_patients` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_givebirth`
+--
+
+DROP TABLE IF EXISTS `tb_givebirth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_givebirth` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `patient_id` int NOT NULL,
+  `birth_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `gestational_age` int DEFAULT '9',
+  `gestational_place` varchar(254) DEFAULT NULL,
+  `gender` varchar(254) DEFAULT NULL,
+  `height` int DEFAULT NULL,
+  `weight` int DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_patient_givebirth` (`patient_id`),
+  CONSTRAINT `fk_patient_givebirth` FOREIGN KEY (`patient_id`) REFERENCES `tb_patients` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_patients`
+--
+
+DROP TABLE IF EXISTS `tb_patients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_patients` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `unique_id` varchar(254) DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `payment` varchar(254) DEFAULT NULL,
+  `wife_nik` bigint DEFAULT NULL,
+  `wife_name` varchar(254) DEFAULT NULL,
+  `wife_blood` varchar(10) DEFAULT NULL,
+  `wife_placedob` varchar(254) DEFAULT NULL,
+  `wife_education` varchar(254) DEFAULT NULL,
+  `husband_nik` bigint DEFAULT NULL,
+  `husband_name` varchar(254) DEFAULT NULL,
+  `husband_blood` varchar(10) DEFAULT NULL,
+  `husband_placedob` varchar(254) DEFAULT NULL,
+  `husband_education` varchar(254) DEFAULT NULL,
+  `religion` varchar(254) DEFAULT 'islam',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_id` (`unique_id`),
+  KEY `FK_PATIENT_ADDED_BY_USER` (`user_id`),
+  CONSTRAINT `FK_PATIENT_ADDED_BY_USER` FOREIGN KEY (`user_id`) REFERENCES `tb_users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_users`
+--
+
+DROP TABLE IF EXISTS `tb_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `unique_id` varchar(254) DEFAULT NULL,
+  `email` varchar(254) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(254) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_id` (`unique_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-14  0:11:40
